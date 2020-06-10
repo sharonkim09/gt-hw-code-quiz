9f7505100c5ac88a669b97e06ce5d1d0ac93992b
Sharon Kim <sharonkim1709@gmail.com>
2020-06-10 18:05:54 -0400

    got timer to countdown

--- script.js
+++ script.js
@@ -3,13 +3,30 @@
 //work on start button and clock starts when clicked..
 var startButton = document.getElementById("start-button");
 var mainPage = document.getElementById("main-page");
+//timer var
+var timeEl = document.querySelector(".time");
+var secondsLeft=75;
 //Define functions
 function startQuiz(){
     console.log("Hello");
 }
+
+//from class activity 8
+function setTime(){
+    var timerInterval= setInterval(function(){
+        secondsLeft--;
+        timeEl.textContent= secondsLeft;
+        //stops timer from running in background
+        if(secondsLeft === 0){
+            clearInterval(timerInterval);
+        }
+    },1000);
+}
+
+
 startQuiz();
 //Call Functions
-
+setTime();
 //Add Events
 startButton.addEventListener("click",startQuiz);
 // console.log(startButton);
