f5d309cb9b81cbfb32294feb2478b9c74e4606b8
Sharon Kim <sharonkim1709@gmail.com>
2020-06-12 16:08:03 -0400

    able to select last button and hide questions

--- script.js
+++ script.js
@@ -4,28 +4,27 @@
 var startButtonEl = document.querySelector("#start-button");
 var mainEl = document.querySelector("#main-page");
 var quizSectionEl = document.querySelector("#quiz-section");
-var quizQuestionsEl= document.querySelector("#questions");
-var questionsEl= document.querySelector("#choices");
-var choice1El= document.querySelector("#choice1");
-var choice2El= document.querySelector("#choice2");
-var choice3El= document.querySelector("#choice3");
-var choice4El= document.querySelector("#choice4");
-var resultSectionEl= document.querySelector("#result-section");
-var highscoreEl= document.querySelector("#highscore-section")
-var backButtonEl= document.querySelector("#back-button");
-var clearButtonEl= document.querySelector("#clear-button");
-var responseEl = document.querySelector("#response")
-var quizScoreEl =document.querySelector("#quiz-score");
+var quizQuestionsEl = document.querySelector("#questions");
+var questionsEl = document.querySelector("#choices");
+var choice1El = document.querySelector("#choice1");
+var choice2El = document.querySelector("#choice2");
+var choice3El = document.querySelector("#choice3");
+var choice4El = document.querySelector("#choice4");
+var resultSectionEl = document.querySelector("#result-section");
+var highScoresSectionEl = document.querySelector("#highscore-section");
+var backButtonEl = document.querySelector("#back-button");
+var clearButtonEl = document.querySelector("#clear-button");
+var responseEl = document.querySelector("#response");
+var quizScoreEl = document.querySelector("#quiz-score");
+var timeRemainingEl = document.querySelector("#time");
 //timer var
 var timeEl = document.querySelector(".time");
 var secondsLeft = 75;
 //highscore variable
-var highScoresEl= document.querySelector("#highScores-button");
-//set initial index of question to 0 
+//var highScoresEl= document.querySelector("#highScores-button");
+//set initial index of question to 0
 var questionIndex = 0;
 var timerRunning = false;
-
-
 
 // define questions in a single object with key-value
 var questions = [
@@ -79,7 +78,7 @@ var currentQuestion = questions[questionIndex];
 //   console.log("Hello");
 // }
 
-//timer starts and displays question... 
+//timer starts and displays question...
 //from class activity 8
 function startQuiz() {
   //timer is off and  need to turn on when starting
@@ -89,8 +88,8 @@ function startQuiz() {
     timeEl.textContent = secondsLeft;
     //stops timer from running in background
     if (secondsLeft === 0) {
-      //need to stop timer when 0 
-      timerRunning= false;
+      //need to stop timer when 0
+      timerRunning = false;
       clearInterval(timerInterval);
       //need to display result section
     }
@@ -100,12 +99,12 @@ function startQuiz() {
 /**
  * When starting quiz, other sections are hidden and quiz questions will be displayed
  */
-function displayQuestions(){
+function displayQuestions() {
   //when i start quiz, everything should be hidden except quiz
   //need to display question choices on the button
   mainEl.style = "display:none";
   resultSectionEl.style = "display:none";
-  highScoresEl.style ="display:none";
+  highScoresSectionEl.style = "display:none";
 
   currentQuestion = questions[questionIndex];
   quizQuestionsEl.textContent = currentQuestion.question;
@@ -114,55 +113,59 @@ function displayQuestions(){
   choice3El.textContent = currentQuestion.choice3;
   choice4El.textContent = currentQuestion.choice4;
 }
-//now to let user to select the questions buttons 
-  function selectedChoice(event, currentQuestion){
-    event.preventDefault;
-    var choiceBtn = event.target;
-    //console.log(choiceBtn);
-    //next question
-    questionIndex++;
+//now to let user to select the questions buttons
+function selectedChoice(event, currentQuestion) {
+  event.preventDefault;
+  var choiceBtn = event.target;
+  //console.log(choiceBtn);
+  //next question
+  questionIndex++;
 
+  //now to compare the selected answer to the answer and display appropriate message
+  if (choiceBtn.getAttribute("id") === currentQuestion.answer) {
+    console.log("correct!");
+    //instead of console logging, set a variable and display on page...
+    responseEl.textContent = "Correct!";
+  } else {
+    console.log("Wrong!");
+    responseEl.textContent = "Wrong!";
+    //deduct time
+    secondsLeft = secondsLeft - 10;
+  }
 
-    //now to compare the selected answer to the answer and display appropriate message
-    if(choiceBtn.getAttribute("id")===currentQuestion.answer){
-      console.log("correct!")
-      //instead of console logging, set a variable and display on page...
-      responseEl.textContent="Correct!";
-    } else {
-      console.log("Wrong!")
-      responseEl.textContent="Wrong!";
-      //deduct time
-      secondsLeft = secondsLeft - 10;
-    }
-    //error on last question .... 
-    //fixing..still working..
-    if(questionIndex === questions.length){
-    timeEl.textContent= secondsLeft;
-      clearInterval(timerInterval);
-    }else{
-      displayQuestions();
-        }
-      }
-      
+  //error on last question ....
+  //fixing..still working..
+  if (questionIndex === questions.length) {
+    // console.log()
+    //hide questions and show results section
+    results();
+
+  } else {
+    displayQuestions();
+  }
+}
+
 // console.log(end)
 
 //to display results section with time and string displayed and hidng other content
 //still working around
-function finished(){
-  mainEl.style = "display:none";
-  quizSectionEl.style = "display:none";
-  quizScoreEl.textContent= secondsLeft;
+function results() {
+  quizScoreEl.textContent = secondsLeft;
+  console.log(secondsLeft);
+    mainEl.style = "display:none";
+    quizSectionEl.style = "display:none";
+    highScoresSectionEl.style = "display:none";
 }
 
 //make the user be able to click quiz answers
-questionsEl.addEventListener("click", function(){
-  selectedChoice(event, currentQuestion)
-})
+questionsEl.addEventListener("click", function () {
+  selectedChoice(event, currentQuestion);
+});
 
 //when we reach end of quiz, hide all and show result section
-function results(){
-quizScoreEl.textContent = secondsLeft;
-}
+// function results(){
+// quizScoreEl.textContent = secondsLeft;
+// }
 
 //
 
